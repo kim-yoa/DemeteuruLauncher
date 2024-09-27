ccd1c67abab3f9dbd4d05165f01752942dd1a828
이호형 <75205559+kim-yoa@users.noreply.github.com>
2024-09-28 05:04:10 +0900

    demetru -> demeteuru

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -7,7 +7,7 @@ const logger = LoggerUtil.getLogger('ConfigManager')
 
 const sysRoot = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
-const dataPath = path.join(sysRoot, '.demetrulauncher')
+const dataPath = path.join(sysRoot, '.demeteurulauncher')
 
 const launcherDir = require('@electron/remote').app.getPath('userData')
 
